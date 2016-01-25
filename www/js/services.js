23890591479c59f04ea0ef01aa9bc0e13b5ddad1
oriolbv <oriolbv@gmail.com>
2016-01-25 10:54:37 +0100

    http request test

--- www/js/services.js
+++ www/js/services.js
@@ -1,35 +1,39 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('Chats', function($http) {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+//  var chats = [{
+//    id: 0,
+//    name: 'Ben Sparrow',
+//    lastText: 'You on your way?',
+//    face: 'img/ben.png'
+//  }, {
+//    id: 1,
+//    name: 'Max Lynx',
+//    lastText: 'Hey, it\'s me',
+//    face: 'img/max.png'
+//  }, {
+//    id: 2,
+//    name: 'Adam Bradleyson',
+//    lastText: 'I should buy a boat',
+//    face: 'img/adam.jpg'
+//  }, {
+//    id: 3,
+//    name: 'Perry Governor',
+//    lastText: 'Look at my mukluks!',
+//    face: 'img/perry.png'
+//  }, {
+//    id: 4,
+//    name: 'Mike Harrington',
+//    lastText: 'This is wicked good ice cream.',
+//    face: 'img/mike.png'
+//  }];
+  var chats = [];
+  $http.get('http://api.openweathermap.org/data/2.5/weather?q=barcelona,es&APPID=155de3b370efd8e0f45c05494dfb08e9').then(function(result) {
+    chats.push(result.data);
+  });
 
   return {
     all: function() {
